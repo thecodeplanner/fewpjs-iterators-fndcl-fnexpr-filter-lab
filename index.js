78d4bddfb7e4807cfc8b79b270a7b04ce6dfd050
thecodeplanner <thecodeplanner@gmail.com>
2020-12-16 21:43:36 -0500

    commit tree 9af1424d33f8ce5539487d114e3abb203bd829c0

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+    return drivers.filter( possibleMatch =>
+      possibleMatch.toLowerCase() === name.toLowerCase()
+    )
+  }
+
+// function fuzzyMatch(drivers, name) {
+
+// }
+
+function matchName(drivers, name) {
+    return drivers.filter(element => element.name === name )
+}
